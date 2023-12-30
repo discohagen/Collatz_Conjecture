2198ca4520824e0174ab64ad087d8b960bb95a7d
Discohagen <leonziegenhagen22@gmail.com>
2023-12-30 16:11:50 +0100

    split logic into functions

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import turtle
+import tkinter
 
 def collatz_conjecture(number: int) -> [int]:
     if number <= 1:
@@ -16,25 +17,10 @@ def collatz_conjecture(number: int) -> [int]:
 
     return conjecture
 
-def plot_list_into_turtle() -> None:
-    turtle.title("Collatz Conjecture")
+def plot_list_into_turtle(coordinates: [int], title = "", showNumbers = True) -> None:
+    turtle.title(title)
     turtle.bgcolor("#1f1f1f")
     s = turtle.getscreen()
-
-    #TODO: Get starting number through tkinter GUI with more info on the conjecture / problem
-    #      and maybe a clickable hyperlink to the Wikipedia.
-    #      Only start turtle plotting when the input got checked.
-    starting_number_string = s.textinput(
-        "Collatz Conjecture Input", 
-        "Please input a starting number for the conjecture. \n" +
-        "The number should be an integer between 1 and 10^15.")
-    
-    try:
-        starting_number = int(starting_number_string)
-    except:
-        raise ValueError("Input must be an integer between 1 and 10^15.")
-    
-    coordinates = collatz_conjecture(starting_number)
     s.setworldcoordinates(0, 0, len(coordinates), 1.1 * max(coordinates))
     t = turtle.getturtle()
     t.hideturtle()
@@ -44,10 +30,31 @@ def plot_list_into_turtle() -> None:
         t.pencolor("#9cdcfe")
         t.goto(i, coordinates[i])
         t.dot(5)
-        t.pencolor("white")
-        t.write(arg=coordinates[i], align="center")
+        if (showNumbers):
+            t.pencolor("white")
+            t.write(arg=coordinates[i], align="center")
         t.down()
 
-    s.mainloop()
+def plot_collatz_conjecture() -> None:
+    window = tkinter.Tk()
+    window.title("Collatz Conjecture Input")
+    tkinter.Label(window, text=
+        "Please input a starting number for the conjecture. \n" +
+        "The number should be an integer between 1 and 10^15.").pack()
+    input = tkinter.Entry(window)
+    input.pack()
+    
+    def on_click() -> None:
+        try:
+            starting_number = int(input.get())
+        except:
+            raise ValueError("Input must be an integer between 1 and 10^15.")
+        window.destroy()
+        plot_list_into_turtle(collatz_conjecture(starting_number), "Collatz Conjecture")
+        return 
+    
+    tkinter.Button(window, text="OK", command=on_click).pack()
 
-plot_list_into_turtle()
+    window.mainloop()
+
+plot_collatz_conjecture()
